e30771cdd852b53344f19aad44e220bc02b726f7
Kevin <mail.kevinpatel@gmail.com>
2019-05-27 19:22:06 -0400

    completed photogur part2

--- Photogur_part1/photogur/models.py
+++ Photogur_part1/photogur/models.py
@@ -8,7 +8,6 @@ class Picture(models.Model):
     def __str__(self):
         return self.title
 
-        
 class Comment(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
--- Photogur_part1/photogur/views.py
+++ Photogur_part1/photogur/views.py
@@ -7,3 +7,10 @@ def pictures_page(request):
     context = {'db_pictures': pic_links }
     response = render(request, 'pictures.html', context)
     return HttpResponse(response)
+
+def picture_show(request, id):
+    picture = Picture.objects.get(pk=id)
+    context = {'picture': picture}
+    response = render(request, 'picture.html', context)
+    return HttpResponse(response)
+    
