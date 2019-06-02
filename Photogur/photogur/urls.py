dba0ca72c9e729bfce1de7dbe074357855d8f31d
Kevin <mail.kevinpatel@gmail.com>
2019-06-02 14:14:34 -0400

    created login page

---
+++ Photogur/photogur/urls.py
@@ -0,0 +1,28 @@
+"""photogur URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from photogur.views import (pictures_page, picture_show, root, picture_search, create_comment, login_view)
+
+urlpatterns = [
+    path('', root),
+    path('admin/', admin.site.urls),
+    path('pictures/', pictures_page, name="pictures_page"),
+    path('pictures/<int:id>', picture_show, name='picture_details'),
+    path('search', picture_search, name='picture_search'),
+    path('comments/new', create_comment, name='create_comment'),
+    path('login/', login_view, name='login'),
+]
---
+++ Photogur/photogur/views.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.decorators.http import require_http_methods
+from photogur.models import Picture, Comment
+from photogur.forms import LoginForm
+
+def pictures_page(request):
+    pic_links = Picture.objects.all()
+    context = {'db_pictures': pic_links }
+    response = render(request, 'pictures.html', context)
+    return HttpResponse(response)
+
+def root(request):
+    return HttpResponseRedirect('/pictures')
+
+def picture_show(request, id):
+    picture = Picture.objects.get(pk=id)
+    context = {'picture': picture}
+    response = render(request, 'picture.html', context)
+    return HttpResponse(response)
+
+def picture_search(request):
+    query = request.GET['query']
+    search_results = Picture.objects.filter(artist=query)
+    context = {'pictures': search_results, 'query': query}
+    response = render(request, 'search.html', context)
+    return HttpResponse(response)
+
+@require_http_methods(['POST'])
+def create_comment(request):
+    user_name = request.POST['name']
+    user_message = request.POST['message']
+    user_select_picture = request.POST['picture']
+    select_picture = Picture.objects.get(id=user_select_picture)
+    comment = Comment(name=user_name, picture=select_picture, message=user_message)
+    comment.save()
+    return redirect("picture_details", id=user_select_picture)
+
+def login_view(request):
+    form = LoginForm()
+    context = {'form': form}
+    http_response = render(request, 'login.html', context)
+    return HttpResponse(http_response)
